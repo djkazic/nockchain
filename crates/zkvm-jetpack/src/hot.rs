0a7ec0a1e3e77b32b549ea45d87a4ded32604656
djkazic <kevincai96@gmail.com>
2025-05-30 19:41:10 -0400

    jets: bp-ifft

--- crates/zkvm-jetpack/src/hot.rs
+++ crates/zkvm-jetpack/src/hot.rs
@@ -464,6 +464,20 @@ pub const BASE_POLY_JETS: &[HotEntry] = &[
         1,
         bp_fft_jet,
     ),
+    (
+        &[
+            K_138,
+            Left(b"one"),
+            Left(b"two"),
+            Left(b"tri"),
+            Left(b"qua"),
+            Left(b"pen"),
+            Left(b"zeke"),
+            Left(b"bp-ifft"),
+        ],
+        1,
+        bp_ifft_jet,
+    ),
 ];
 
 pub const ZTD_JETS: &[HotEntry] = &[(
